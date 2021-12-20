06e5c7aa9d8c2f4fee14d9c33ccfad173ea43314
kate-kudr <kul197409@gmail.com>
2021-12-20 16:05:05 +0300

    all functions, correct division into categories

--- scheduler.h
+++ scheduler.h
@@ -3,14 +3,18 @@
 
 #include "config.h"
 
-//EDF
-int earliest_deadline;
-int earliest_deadline_index;
+//both
 int remains_capacity[MAX_PROCESSES];
 int next_deadline[MAX_PROCESSES];
 int process_new_period[MAX_PROCESSES];
 
+//EDF
+int earliest_deadline;
+int earliest_deadline_index;
+
 int get_earliest_deadline();
+
+void check_new_period_edf();
 
 //RMS
 int shortest_period;
@@ -18,6 +22,6 @@ int shortest_period_index;
 
 int get_shortest_period();
 
-void func1();
+void check_new_period_rms();
 
 #endif //SCHEDULER_H
